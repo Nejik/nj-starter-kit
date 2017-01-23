c5336f080081f063063c54ffb7db0befcb869685
Dmitry Lomako <dmitry.lomako@softeq.com>
2017-01-23 17:32:41 +0300

    *added hmtl paths to project config

--- configs/project.config.js
+++ configs/project.config.js
@@ -13,17 +13,24 @@ const isVerbose = argv.verbose;
 const isDevelopment = argv.env !== "production";
 
 let paths = {
+  dist: dist,
+  publicPath: '/',
+
+  html: {
+    src: root.resolve('src/index.html'),
+    dist: dist,
+    watch: [root.resolve('src/**/*.html')]
+  },
   root: root.toString(),
   src: src,
   components: components,
   css: css,
   img: img,
   vendor: vendor,
-  dist: dist,
-  publicPath: '/',
 
   isVerbose: isVerbose,
   isDevelopment: isDevelopment
 }
+console.log(paths.html)
 
 module.exports = paths;
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 const config = require('./configs/project.config.js');
 const webpackConfig = require('./configs/webpack.config.js');
+const postcssConfig = require('./configs/postcss.config.js');
 
 //common
 const del = require('del');
 const path = require('path')
 const gulp = require('gulp');
 const gulpIf = require('gulp-if');
+const sourcemaps = require('gulp-sourcemaps');
 
 
 //styles
-const postcssConfig = require('./configs/postcss.config.js');
-
 const postcss = require('gulp-postcss');
-const sourcemaps = require('gulp-sourcemaps');
 
 //js
 const webpack = require('webpack');
@@ -26,7 +25,12 @@ gulp.task('clean', function () {
   return del([config.src])
 })
 
-gulp.task('styles', function () {
+gulp.task('html', function () {
+  return gulp .src(config.html.src)
+              .pipe(gulp.dest(config.html.dist))
+})
+
+gulp.task('css', function () {
   return gulp .src('src/css/styles.css')
               .pipe(gulpIf(config.isDevelopment, sourcemaps.init()))
               .pipe(postcss(postcssConfig))
@@ -35,16 +39,22 @@ gulp.task('styles', function () {
               .pipe(gulpIf(config.isDevelopment, bs.stream()))
 })
 
+gulp.task('webpack', function (gulpCallback) {
+  webpack(webpackConfig, function() {
+    gulpCallback();
+  });
+})
+
 gulp.task('watch', function () {
   // gulp.watch(paths.html.watch, gulp.series('html'));
   // gulp.watch(paths.html.watch_partials, gulp.series('html_partials'));
 
-  gulp.watch('src/**/*.css', gulp.series('styles')); 
+  gulp.watch('src/**/*.css', gulp.series('css')); 
   // gulp.watch(paths.jsVendor.watch, gulp.series('js:vendor')); //js bundled by webpack and webpack has own watcher, but vendors concataneted by gulp
   // gulp.watch(paths.images.watch, gulp.series('images'));
 })
 
-gulp.task('serve', function (cb) {
+gulp.task('serve', function (cb) {//serve contains js task, because of webpack integration
   const compiler = webpack(webpackConfig);
 
   const webpackDevMiddleware = require('webpack-dev-middleware')(compiler, {
@@ -57,8 +67,6 @@ gulp.task('serve', function (cb) {
       bs.reload()
     } else {
       serverStarted = true;
-      gulp.src('./src/index.html')
-        .pipe(gulp.dest(config.dist))
 
       bs.init({
         open: false,
@@ -87,6 +95,6 @@ gulp.task('serve', function (cb) {
 
 
 
+gulp.task('build', gulp.parallel('html','css'))
 
-
-gulp.task('default', gulp.parallel('serve','watch'))
+gulp.task('default', gulp.series('build', gulp.parallel('serve','watch')))
