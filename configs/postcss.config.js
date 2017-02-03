754282c70f48f68807ee340b9d19d1e97354076a
Dmitry Lomako <dmitry.lomako@softeq.com>
2017-02-03 15:08:02 +0300

    +added css minify to production build

--- configs/postcss.config.js
+++ configs/postcss.config.js
@@ -3,12 +3,12 @@ const config = require('./project.config');
 // const cssReporter = require('postcss-reporter');
 const cssImport = require('postcss-import');
 const cssNext = require('postcss-cssnext');
-// const cssNano = require('cssnano');
+const cssNano = require('cssnano');
 const cssMqPacker = require('css-mqpacker');
 const cssInlineSvg = require('postcss-inline-svg');
 
 
-module.exports = [
+let postcssConfig = [
   cssImport({
     root: config.root,
     path: [// node_modules exists in resolve paths by default and we don't need to place it here
@@ -18,7 +18,7 @@ module.exports = [
     ]
   }),
   cssNext,
-  cssInlineSvg({path: config.src}),
+  cssInlineSvg({ path: config.src }),
   cssMqPacker({
     sort: true
   }),
@@ -39,4 +39,12 @@ module.exports = [
   //   },
   //   clearMessages: false
   // })
-];
+]
+
+if (!config.isDevelopment) {
+    postcssConfig.push(cssNano({
+                             safe:true,
+                             autoprefixer:false//autoprefixer in cssNano works in delete mode, while in cssNext in add mode. Disable delete mode.
+                            }))
+  }
+module.exports = postcssConfig;
